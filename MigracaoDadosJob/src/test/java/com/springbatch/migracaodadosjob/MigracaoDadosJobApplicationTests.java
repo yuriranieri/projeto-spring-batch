3e490dc6636039a7e896097887d0c275be6ea5b4
yuri.ranieri <yuri.ranieri@compasso.com.br>
2022-02-22 14:13:48 -0300

    projeto MigracaoDadosJob criado

---
+++ MigracaoDadosJob/src/test/java/com/springbatch/migracaodadosjob/MigracaoDadosJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.migracaodadosjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigracaoDadosJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
